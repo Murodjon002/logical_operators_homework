6a783c032e89e51592c2a61f57f3e6b47db8d7a5
Murodjon <murodjondeveloper@gmail.com>
2021-12-29 00:26:42 +0500

    logic18

--- logic18.py
+++ logic18.py
@@ -5,4 +5,4 @@ def main(a):
     Returns:
         True if all digits of a are in descending order, False otherwise
     """
-    return
+    return (a//10000<a//1000%10) and (a//1000%10<a//100%10) and (a//100%10<a//10%10) and (a//10%10<a%10)
