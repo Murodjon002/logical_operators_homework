b934a8005a240a1cfc258bfa865ab0e696687de6
Murodjon <murodjondeveloper@gmail.com>
2021-12-29 00:25:21 +0500

    logic17 change

--- logic17.py
+++ logic17.py
@@ -6,4 +6,4 @@ def main(a):
     Returns:
         True if all digits of a are in ascending order, False otherwise
     """
-    return (a//10000<a//1000%10) and (a//1000%10<a//100%10) and (a//100%10<a//10%10) and (a//10%10<a%10)
+    return (a//10000>a//1000%10) and (a//1000%10>a//100%10) and (a//100%10>a//10%10) and (a//10%10>a%10)
